3026c2ca4f27aa9442ff2d55106a60591986020f
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-08-21 17:35:08 -0500

    migration worked

--- backend/migrations/20210726171949-create-comment.js
+++ backend/migrations/20210726171949-create-comment.js
@@ -1,43 +1,39 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('users', 'password_digest', {
-      type:Sequelize.DataTypes.STRING
-    })
-    // await queryInterface.createTable('comments', {
-    //   comment_id: {
-    //     allowNull: false,
-    //     autoIncrement: true,
-    //     primaryKey: true,
-    //     type: Sequelize.INTEGER
-    //   },
-    //   place_id: {
-    //     type: Sequelize.SMALLINT
-    //   },
-    //   stars: {
-    //     type: Sequelize.FLOAT
-    //   },
-    //   content: {
-    //     type: Sequelize.STRING
-    //   },
-    //   rant: {
-    //     type: Sequelize.BOOLEAN
-    //   },
-    //   author_id: {
-    //     type: Sequelize.SMALLINT
-    //   },
-    //   created_at: {
-    //     allowNull: false,
-    //     type: Sequelize.DATE
-    //   },
-    //   updated_at: {
-    //     allowNull: false,
-    //     type: Sequelize.DATE
-    //   }
-    // });
+    await queryInterface.createTable('comments', {
+      comment_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      place_id: {
+        type: Sequelize.SMALLINT
+      },
+      stars: {
+        type: Sequelize.FLOAT
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      rant: {
+        type: Sequelize.BOOLEAN
+      },
+      author_id: {
+        type: Sequelize.SMALLINT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
   },
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('users','password_digest')
-    // await queryInterface.dropTable('comments');
+    await queryInterface.dropTable('comments');
   }
 };
